13716b379190e6c34a7f307f63871e4de7d7548a
TimyxiaoA <1012177593@qq.com>
2021-04-12 16:23:55 +0800

    home初始化

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 import Vue from 'vue'
 import App from './App'
 
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 基准路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest  = function () {
+  uni.hideLoading()
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
